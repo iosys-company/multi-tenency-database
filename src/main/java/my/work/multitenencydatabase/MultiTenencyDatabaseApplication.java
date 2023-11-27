9613ee8b892cf5950db92e98c9243e080e8859fd
IOSYS <118172294+iosys-hub@users.noreply.github.com>
2023-11-27 19:05:19 +0900

    등록

---
+++ src/main/java/my/work/multitenencydatabase/MultiTenencyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package my.work.multitenencydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenencyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenencyDatabaseApplication.class, args);
+	}
+
+}
