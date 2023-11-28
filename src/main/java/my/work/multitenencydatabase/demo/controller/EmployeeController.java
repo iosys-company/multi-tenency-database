de0e486d39ac30ada6f7c09a4640d5775cdf1a7c
IOSYS <118172294+iosys-hub@users.noreply.github.com>
2023-11-28 14:56:13 +0900

    Mybatis조회추가

--- src/main/java/my/work/multitenencydatabase/demo/controller/EmployeeController.java
+++ src/main/java/my/work/multitenencydatabase/demo/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package my.work.multitenencydatabase.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,10 @@ public class EmployeeController {
     @PostMapping
     public ResponseEntity<Employee> save(@RequestBody Employee employee) {
         return ResponseEntity.ok().body(employeeService.save(employee));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok().body(employeeService.findAll());
     }
 }
---
+++ src/main/java/my/work/multitenencydatabase/demo/mapper/EmployeeMapper.java
@@ -0,0 +1,12 @@
+package my.work.multitenencydatabase.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import my.work.multitenencydatabase.demo.dto.EmployeeDto;
+
+@Mapper
+public interface EmployeeMapper {
+    List<EmployeeDto> findAll();
+}
